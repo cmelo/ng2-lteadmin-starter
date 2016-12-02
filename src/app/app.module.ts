ba3468c72450f484cb45a481670a47c279e20400
cmelo <m.celin@gmail.com>
2016-12-02 15:56:21 +0100

    lte admin demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+
+import { LoginComponent } from './+login/login.component';
+import { AuthorizedComponent } from './+authorized/authorized.component';
+import { DashboardComponent } from './+authorized/+dashboard/dashboard.component';
+import { PageComponent } from './+authorized/+page/page.component';
+
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: AuthorizedComponent, children: [
+    { path: '', component: DashboardComponent },
+    { path: 'page', component: PageComponent },
+  ] },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    DashboardComponent,
+    PageComponent,
+    LoginComponent,
+    AuthorizedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    Ng2BootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
